08cfa4bb0d1f8a5bcbdccb9fe4410989673966cf
8x15yz <8x15yz@gmail.com>
2022-04-20 20:52:32 +0900

    0420/ 모델 변경 : 게시글+코멘트 기능 추가

--- accounts/models.py
+++ accounts/models.py
@@ -2,4 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+
+class Article(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(User, related_name='like_articles')
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
