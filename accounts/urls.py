d5242128cf3819b512d9d1b3707c71e1cbc563f6
8x15yz <8x15yz@gmail.com>
2022-04-25 00:30:22 +0900

    0425/ 비회원 노출페이지 추가 + view함수 구조변경 etc

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,17 +1,18 @@
 from django.urls import path
-from . import views
+from .views import index_to_comment
 
 app_name = 'accounts'
 urlpatterns = [
-    path('userindex/', views.userindex, name='userindex'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('profile/<str:username>/', views.profile, name='profile'),
-    path('create/', views.create, name='create'),
-    path('profile/<str:username>/retrieve/<int:p_pk>/', views.retrieve, name='retrieve'),
+    path('forAnonymousUser/', index_to_comment.forAnonymousUser, name='forAnonymousUser'),
+    path('userindex/', index_to_comment.userindex, name='userindex'),
+    path('signup/', index_to_comment.signup, name='signup'),
+    path('login/', index_to_comment.login, name='login'),
+    path('logout/', index_to_comment.logout, name='logout'),
+    path('profile/<str:username>/', index_to_comment.profile, name='profile'),
+    path('create/', index_to_comment.create, name='create'),
+    path('profile/<str:username>/retrieve/<int:p_pk>/', index_to_comment.retrieve, name='retrieve'),
 
     # 댓글로직
-    path('profile/<str:username>/retrieve/<int:p_pk>/comments/', views.comments_create, name='comments_create'),
+    path('profile/<str:username>/retrieve/<int:p_pk>/comments/', index_to_comment.comments_create, name='comments_create'),
 
 ]
--- accounts/views/index_to_comment.py
+++ accounts/views/index_to_comment.py
@@ -1,22 +1,35 @@
 from django.shortcuts import render
-from .models import User, Article, Comment
+from ..models import User, Article, Comment
 from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 from django.contrib.auth import get_user_model
-from .forms import CustomUserCreationForm, ArticleCreationForm, CommentCreationForm
+from ..forms import CustomUserCreationForm, ArticleCreationForm, CommentCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
 
 
-# Create your views here.
+# 로그인 안했을때 사이트 소개 페이지
+def forAnonymousUser(request):
+    return render(request, 'accounts/openingpage.html')
+
+
+# 유저목록 보여주는 페이지 (이후 팔로우기능 추가되면 바뀔예정)
 def userindex(request):
+    if not request.user.is_authenticated:
+        return redirect('accounts:forAnonymousUser')
+
     userindexs = User.objects.all()
     context = {
         'userindexs':userindexs
     }
     return render(request, 'accounts/userindex.html', context)
 
+
+# 개인계정 페이지 로드
 def profile(request, username):
+    if not request.user.is_authenticated:
+        return redirect('accounts:forAnonymousUser')
+    
     user_name = get_object_or_404(User, username=username)
     user_pk = User.objects.filter(username=username).values('id').get().get('id')
     user_articles = Article.objects.filter(user_id=user_pk).order_by('-pk')
@@ -26,7 +39,12 @@ def profile(request, username):
     }
     return render(request, 'accounts/profile.html', context)
 
+
+# 회원가입 페이지 로드
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect('accounts:userindex')
+
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         form.save()
@@ -38,7 +56,12 @@ def signup(request):
     }
     return render(request, 'accounts/signup.html', context)
 
+
+# 로그인 페이지 로드
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('accounts:userindex')
+
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
@@ -52,12 +75,20 @@ def login(request):
     }
     return render(request, 'accounts/login.html', context)
 
+
+# 로그아웃
 def logout(request):
     if request.user.is_authenticated:
         auth_logout(request)
     return redirect('accounts:userindex')
 
+
+
+# 게시글 작성 페이지 로드
 def create(request):
+    if not request.user.is_authenticated:
+        return redirect('accounts:forAnonymousUser')
+
     if request.method == 'POST':
         form = ArticleCreationForm(request.POST)
         if form.is_valid():
@@ -72,7 +103,13 @@ def create(request):
     }
     return render(request, 'accounts/create.html', context)
 
+
+
+# 게시글 디테일 페이지 로드
 def retrieve(request, username, p_pk):
+    if not request.user.is_authenticated:
+        return redirect('accounts:forAnonymousUser')
+        
     article = get_object_or_404(Article, pk=p_pk)
     form = CommentCreationForm()
     comments = article.comment_set.all()
@@ -83,6 +120,9 @@ def retrieve(request, username, p_pk):
     }
     return render(request, 'accounts/detailpage.html', context)
 
+
+
+# 댓글 작성 로직
 def comments_create(request, username, p_pk):
     article = Article.objects.get(pk=p_pk)
     form = CommentCreationForm(request.POST)
