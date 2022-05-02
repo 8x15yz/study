89f7323d89eb966654a84f78d6973aad30fd0e56
8x15yz <8x15yz@gmail.com>
2022-05-02 22:07:11 +0900

    0502/ js first commit ?

---
+++ 01-js-Basic/DomChangeMehod.js
@@ -0,0 +1,9 @@
+// 1. createElement : 요소를 생성해서 반환함
+
+// 2. append 여러개 객체를 추가 + 반환값 없음
+// 3. appendChild 하나만의 객체를 추가 "Node 객체만 허용" + 추가된 Node 객체를 반환함
+
+// 4. innerText 내용 추가
+// 5. innerHTML HTML 마크업 반환
+
+// 6. XSS 공격자가 입력요소를 사용해서 악성 스크립트를 삽입해 공격함
