d11f86dee3d5be0aa5a72a94e6f603caa5b4daef
8x15yz <8x15yz@gmail.com>
2022-05-10 22:56:29 +0900

    0510/ 유튜브 api 공부

---
+++ Vue-js/0510-hws-YouTuBe-API/src/components/TheSerchBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <input type="text" @keyup.enter="search">
+</template>
+
+<script>
+export default {
+  methods: {
+    search(event) {
+    this.$emit('set-input-value', event.target.value)
+    console.log(event.target.value)
+    }
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue-js/0510-hws-YouTuBe-API/src/components/VideoList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      <h2>videoList</h2>
+      <!-- {{ videos }} -->
+      <VideoListItem 
+          v-for="video in videos"
+          :key="video.id.videoId"
+          :video="video"
+          @set-selected-video="setSelectedVideo"/>
+  </div>
+</template>
+
+<script>
+import VideoListItem from '@/components/VideoListItem'
+export default {
+  components: {
+    VideoListItem
+  },
+  props: {
+    videos: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    setSelectedVideo(newSelectedVideo) {
+      this.$emit('set-selected-video', newSelectedVideo)
+      console.log('여기도 됐는디')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
