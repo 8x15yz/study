4769e29cadf58c3187783c204d76e282e848c76e
8x15yz <8x15yz@gmail.com>
2022-05-14 23:39:51 +0900

    완성

--- Vue-js/0509-vue-기본로직/0509-mfa/src/App.vue
+++ Vue-js/0509-vue-기본로직/0509-mfa/src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="d-flex">
+    <div id="app">
+      <h1>here is upper class</h1>
+      <input type="text" 
+      v-model="visualised" 
+      @click="clickedFunction"
+      @keyup.enter="enterdFunction">
+      <p>emit 받은 데이터: {{sendedData}}</p>
+    </div>
+    
+    <TheAbout 
+    :prop-data="propData" 
+    @emit-keyword="sendedEmitFunction"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheAbout from '@/components/TheAbout.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheAbout
+  },
+  data() {
+    return {
+      visualised: '뭔가를 입력해보세요',
+      propData: '',
+      sendedData: ''
+    }
+  },
+  methods: {
+    enterdFunction(event) {
+      this.propData = event.path[0]._value
+    },
+    clickedFunction() {
+      this.visualised=''
+    },
+    sendedEmitFunction(data) {
+      this.sendedData=data
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  outline: 10px;
+  border: 10px solid black;
+  padding: 1rem;
 }
 </style>
