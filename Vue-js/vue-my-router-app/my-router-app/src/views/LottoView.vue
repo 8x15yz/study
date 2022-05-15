c483698223dc6a84501a8e2b038005dc34a11114
8x15yz <8x15yz@gmail.com>
2022-05-15 22:53:41 +0900

    0515/ router

---
+++ Vue-js/vue-my-router-app/my-router-app/src/views/LottoView.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="d-flex m-5 justify-content-evenly">
+      <div id="outLine">
+          <h1>LottoVue</h1>
+          <button
+          @click="selectedNumbers"
+          >
+          get numbers
+          </button>
+          <p>{{selectedNumbers}}</p>
+      </div>
+    
+    <div>
+        <ChartVue />
+    </div>
+  </div>
+</template>
+
+<script>
+import ChartVue from '@/components/ChartVue.vue'
+import _ from 'lodash'
+
+export default {
+    name: 'LottoVue',
+    components: {
+        ChartVue
+    },
+    data() {
+        return {
+            selectedNumbers: null
+        }
+    },
+    methods: {
+    getLuckyNumbers() {
+      this.selectedNumbers = _.sortBy(_.sampleSize(_.range(1, 46), this.$route.params.number))
+    }
+  },
+  created() {
+    console.log('created', this.$route.params.number)
+  },
+  mounted() {
+    console.log('mounted', this.$route.params.number)
+  },
+  updated() {
+    console.log('updated', this.$route.params.number)
+  }
+}
+</script>
+
+<style>
+#outLine {
+  outline: solid green;
+  width: 600px;
+  padding: 2rem;
+}
+</style>
---
+++ Vue-js/vue-my-router-app/my-router-app/src/views/LunchView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="d-flex m-5 justify-content-evenly">
+      <div id="outLine">
+          <h1>LunchView</h1>
+      </div>
+    
+    <div class="">
+        <ChartVue />
+    </div>
+  </div>
+</template>
+
+<script>
+import ChartVue from '@/components/ChartVue.vue'
+
+export default {
+    name: 'LunchView',
+    components: {
+        ChartVue
+    }
+}
+</script>
+
+<style>
+#outLine {
+  outline: solid green;
+  width: 600px;
+  padding: 2rem;
+}
+</style>
